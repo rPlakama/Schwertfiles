30caeb24abf591e60a29afd12da6042f53335b67
rPlakama <rPlakama@proton.me>
2025-04-15 15:50:54 -0300

    adjusted for battery main.nix

--- main.nix
+++ main.nix
@@ -1,51 +1,43 @@
-{ config, pkgs, ... }:
-
+{
+  config,
+  pkgs,
+  ...
+}:
 # O próposito desse IMPORT para o CONFIGURATION.NIX é organizar e reduzir o tamanho do CONFIGURATION.NIX e manter os realmente necessários instalados.
 {
-  
   environment.systemPackages = with pkgs; [
+    #Componentes Principais
 
-#Componentes Principais
+    vim
+    mesa
+    brightnessctl
+    gcc
+    wezterm
 
-  vim 
-  kitty 
-  mesa
-  brightnessctl
-  gcc
+    #Serviços
 
-#Serviços
+    git
+    p7zip
+    unzip
+    yadm
+    home-manager
+  ];
+  # Serviços e configurações diversas
 
-  git
-  p7zip
-  unzip
-  yadm
-  power-profiles-daemon
-  
-#Programas
-
-  firefox
-
-];
-# Serviços e configurações diversas
-
-# Hyprland  
+  # Hyprland
   programs.hyprland.enable = true;
-  programs.hyprlock.enable = true; 
-	
-# power-profiles-daemon
-  services.power-profiles-daemon.enable = true;
+  programs.hyprlock.enable = true;
 
-# SDDM como Display Manager e auto-login
+  # SDDM como Display Manager e auto-login
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.autoLogin.user = "rplakama";
   services.displayManager.defaultSession = "hyprland";
 
-# Configurações NVIM Instalado Via Home_Manager
-programs.neovim.enable = true;
-programs.neovim.defaultEditor = true;
+  # Configurações NVIM Instalado Via Home_Manager
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
 
-# Ativar Bluetooth
-hardware.bluetooth.enable = true;
-
+  # Ativar Bluetooth.
+  hardware.bluetooth.enable = true;
 }
