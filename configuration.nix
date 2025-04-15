7b3e003a1f1e0c630a9dc5c5abc68d3e8a26b777
rPlakama <rPlakama@proton.me>
2025-04-15 15:49:48 -0300

    configuration (generation??).nix

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,17 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }: 
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./main.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./main.nix
+    ./battery.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -21,7 +23,7 @@
   # Set your time zone.
   time.timeZone = "America/Recife";
 
-  # Select internationalisation properties.
+  # Select internationalisation properties./ho
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -37,14 +39,13 @@
   };
 
   boot = {
-
     plymouth = {
       enable = true;
       theme = "rings";
       themePackages = with pkgs; [
         # By default we would install all themes
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
+          selected_themes = ["rings"];
         })
       ];
     };
@@ -63,7 +64,6 @@
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
     loader.timeout = 0;
-
   };
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -78,19 +78,17 @@
   users.users.rplakama = {
     isNormalUser = true;
     description = "rPlakama";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-fonts.packages = with pkgs; [
-
- nerd-fonts.caskaydia-cove
- ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.caskaydia-cove
+  ];
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
