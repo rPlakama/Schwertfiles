2b0ed16c3379c1eaba8696ac455b645bf8e698ec
rPlakama <rPlakama@proton.me>
2025-05-02 19:00:53 -0300

    modified:   .config/home-manager/home.nix 	modified:   .config/hypr/hyprland.conf 	modified:   .config/nvim/lazy-lock.json

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -14,6 +14,7 @@
  # Development Tools & Utilities
 
   nodejs_23
+  wallust
   tidal-hifi
   yarn
   pyright
@@ -27,6 +28,7 @@
   nixd
 
   # System & Configuration Tools
+
   yadm
   brightnessctl
   pwvucontrol
@@ -43,6 +45,7 @@
 
   # Web & Communication Apps
   firefox-bin
+  vivaldi
   discord
   telegram-desktop
   dropbox
@@ -54,53 +57,8 @@
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
+	TERMINAL = "wezterm";
   };
-
-  programs.rofi = {
-    enable = true;
-    font = "Cascadia Code";
-    terminal = "${pkgs.wezterm}/bin/wezterm";
-    theme = let
-      #Theme configuration by Inherit
-      inherit (config.lib.formats.rasi) mkLiteral;
-    in {
-      "*" = {
-        background-color = mkLiteral "transparent";
-        foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
-        border-color = mkLiteral "#000000";
-        border-radius = mkLiteral "0.5%";
-        text-color = mkLiteral "white";
-        width = 700;
-        spacing = 3;
-      };
-	  "#listview" = {
-      spacing = 2; # vertical space between rows
-      };
-
-	   "element" = {
-        padding = mkLiteral "2px 0px";
-      };
-
-	   "#prompt" = {
-        text = "𐖠";  # or "" to hide completely
-        text-color = mkLiteral "white";
-      };
-      "#inputbar" = {
-        children = map mkLiteral ["prompt" "entry"];
-      };
-      "#textbox-prompt-colon" = {
-        expand = false;
-        str = ":";
-        margin = mkLiteral "0px 0.3em 0em 0em";
-        text-color = mkLiteral "white";
-      };
-      "#window" = {
-        background-color = mkLiteral "rgba (0,0,0,0.9)";
-        border-radius = mkLiteral "0.5%";
-        padding = 5;
-      };
-    }; #End Of Inherit
-  }; #End Of Rofi
 
   programs.waybar = {
     enable = true;
@@ -313,6 +271,52 @@
         };
 	  };
     };
+
+ programs.rofi = {
+    enable = true;
+    font = "Cascadia Code";
+    terminal = "${pkgs.wezterm}/bin/wezterm";
+    theme = let
+      #Theme configuration by Inherit
+      inherit (config.lib.formats.rasi) mkLiteral;
+    in {
+      "*" = {
+        background-color = mkLiteral "transparent";
+        foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
+        border-color = mkLiteral "#000000";
+        border-radius = mkLiteral "0.5%";
+        text-color = mkLiteral "white";
+        width = 700;
+        spacing = 3;
+      };
+	  "#listview" = {
+      spacing = 2; # vertical space between rows
+      };
+
+	   "element" = {
+        padding = mkLiteral "2px 0px";
+      };
+
+	   "#prompt" = {
+        text = "𐖠";  # or "" to hide completely
+        text-color = mkLiteral "white";
+      };
+      "#inputbar" = {
+        children = map mkLiteral ["prompt" "entry"];
+      };
+      "#textbox-prompt-colon" = {
+        expand = false;
+        str = ":";
+        margin = mkLiteral "0px 0.3em 0em 0em";
+        text-color = mkLiteral "white";
+      };
+      "#window" = {
+        background-color = mkLiteral "rgba (0,0,0,0.9)";
+        border-radius = mkLiteral "0.5%";
+        padding = 5;
+      };
+    }; 
+  }; 
 
   # GTK + cursor + theme (Corrigido)
 
