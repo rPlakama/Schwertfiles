46622fa88e6219065e3e05e4ea22199e33874fb3
rPlakama <rPlakama@proton.me>
2025-05-12 10:15:12 -0300

    modified:   .config/nvim/lazy-lock.json 	modified:   NixC/nixos/configuration.nix 	modified:   NixC/nixos/main.nix

--- NixC/nixos/main.nix
+++ NixC/nixos/main.nix
@@ -3,24 +3,16 @@
   # Nix Configurations. 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.config.allowUnfree = true;
-
-  hardware.bluetooth.enable = true;
-  boot.loader.timeout = 0;
-  hardware.graphics.enable = true;
-
-  systemd.services.systemd-udev-settle.enable = false;
-  systemd.services.NetworkManager-wait-online.enable = false;
+  hardware = { bluetooth = { enable = true; powerOnBoot = false; }; graphics.enable = true; };
+  systemd.services = { systemd-udev-settle.enable = false; NetworkManager-wait-online.enable = false;};
+  boot.loader = { systemd-boot.enable = true; efi.canTouchEfiVariables = true; timeout = 0;};
 
   # Pkgs:
   environment.systemPackages = with pkgs; [
-
     git
     neovim
     home-manager
-
   ];
-
-  
   fonts.packages = with pkgs; [
     nerd-fonts.caskaydia-cove
     noto-fonts
@@ -33,7 +25,6 @@
   programs = {
   hyprland.enable = true;
   hyprlock.enable = true;
-
   fish.enable = true;
   neovim.enable = true;
   neovim.defaultEditor = true;
@@ -50,6 +41,16 @@
     };
   system76-scheduler.settings.cfsProfiles.enable = true;
   };
+  #Thunar 
+  programs.xfconf.enable = true;
+  programs.thunar.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-volman
+    thunar-archive-plugin
+    thunar-media-tags-plugin
+  ];
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
 
   nix.gc = {
     automatic = true;
