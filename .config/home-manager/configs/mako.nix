923bb11b722de491b2909c7d2bf08564897f4af6
rPlakama <rPlakama@proton.me>
2025-05-20 18:45:05 -0300

    new file:   .config/home-manager/configs/fastfetch.nix 	new file:   .config/home-manager/configs/fish.nix 	new file:   .config/home-manager/configs/mako.nix 	new file:   .config/home-manager/configs/rofi.nix 	new file:   .config/home-manager/configs/starship.nix 	new file:   .config/home-manager/configs/waybar.nix 	new file:   .config/home-manager/configs/yazi.nix 	new file:   .config/home-manager/stylix.nix

---
+++ .config/home-manager/configs/mako.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  services.mako = {
+    enable = true;
+    font = "cascadia code 8";
+    layer = "overlay";
+    borderColor = "#808080";
+    backgroundColor = "#CC000000";
+    borderRadius = 3;
+    defaultTimeout = 5000;
+    borderSize = 0;
+  };
+}
