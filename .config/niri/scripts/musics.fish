69df36a9361c7764420bd4682a789c79bf4003ad
rPlakama <rPlakama@proton.me>
2025-09-24 12:06:10 -0300

    modified:   .config/niri/config.kdl 	new file:   .config/niri/scripts/musics.fish 	new file:   .config/niri/scripts/vim/undodir/%home%rplakama%.config%niri%scripts%musics.fish 	new file:   .config/niri/scripts/vim/undodir/%home%rplakama%.config%niri%scripts%musics.sh 	modified:   .config/niri/vim/undodir/%home%rplakama%.config%niri%config.kdl 	new file:   .config/niri/vim/undodir/%home%rplakama%.config%niri%scripts%musics.sh 	modified:   .config/waybar/config.jsonc 	modified:   .config/waybar/style.css 	modified:   .config/waybar/vim/undodir/%home%rplakama%.config%waybar%style.css

---
+++ .config/niri/scripts/musics.fish
@@ -0,0 +1,9 @@
+#!/run/current-system/sw/bin/fish
+cd ~;
+while true
+    set file (fd --full-path ~/download/ -d 3 | fzf)
+    if test -z "$file"
+        break
+    end
+    mpv --no-save-position-on-quit "$file"
+end
